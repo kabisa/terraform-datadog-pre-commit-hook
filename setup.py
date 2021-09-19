d997a1e7b40cbfd6a7960fe35685d6d419bce579
Sjuul Janssen <sjuul@outlook.com>
2021-09-19 12:31:28 +0200

    gh-md-toc

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     install_requires=['inflection', 'pyhcl'],
     entry_points={
         'console_scripts': ['tf_datadog_docs=tf_datadog_docs:main'],
-    }
+    },
+    scripts=[
+        'gh-md-toc'
+    ]
 )

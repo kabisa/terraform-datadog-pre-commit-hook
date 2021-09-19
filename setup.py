09fcf37bcc8788fa75f1dabb97a81f7730b0f634
Sjuul Janssen <sjuul@outlook.com>
2021-09-19 12:07:07 +0200

    package

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+    packages=['tf_datadog_docs'],
     install_requires=['inflection', 'pyhcl'],
     entry_points = {
         'console_scripts': ['tf_datadog_docs=tf_datadog_docs:main'],

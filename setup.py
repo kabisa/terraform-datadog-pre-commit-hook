1db83c4f8f0cf929877c7193ce87cd24299198a5
Sjuul Janssen <sjuul@outlook.com>
2021-09-19 12:10:19 +0200

    docstring

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+# I've tried pyproject.toml but console_scripts weren't supported yet
 setup(
     packages=['tf_datadog_docs'],
     install_requires=['inflection', 'pyhcl'],

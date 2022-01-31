700a83a65c407aef9e09492afaefcefe3c33e05b
Sjuul Janssen <sjuul@outlook.com>
2022-01-31 15:08:47 +0100

    added pyyaml dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # I've tried pyproject.toml but console_scripts weren't supported yet
 setup(
     packages=["tf_datadog_docs"],
-    install_requires=["inflection", "pyhcl"],
+    install_requires=["inflection", "pyhcl", "pyyaml"],
     entry_points={
         "console_scripts": [
             "tf_datadog_docs=tf_datadog_docs:main",

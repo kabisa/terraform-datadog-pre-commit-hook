794ac812b8e90ae86c0ba8f6230b313671121ebb
Sjuul Janssen <sjuul@outlook.com>
2022-02-02 08:59:16 +0100

    moving to python-hcl2

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 # I've tried pyproject.toml but console_scripts weren't supported yet
 setup(
     packages=["tf_datadog_docs"],
-    install_requires=["inflection", "pyhcl", "pyyaml"],
+    install_requires=["inflection", "python-hcl2", "pyyaml"],
     entry_points={
         "console_scripts": [
             "tf_datadog_docs=tf_datadog_docs:main",
-            "tf_datadog_docs_index=tf_datadog_docs:add_to_local_index"
+            "tf_datadog_docs_index=tf_datadog_docs:add_to_local_index",
         ],
-    }
+    },
 )
